c60773d8161cb310c944eb7eb6912c53c1fc2ab0
Myacinech <mohamedyacine.chrigui@esprit.tn>
2024-04-21 11:21:34 +0100

    modification de gestion de projet

--- src/main/java/com/test/COCONSULT/Services/SMSService.java
+++ src/main/java/com/test/COCONSULT/Services/SMSService.java
@@ -3,14 +3,20 @@ package com.test.COCONSULT.Services;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SMSService {
 
-    public static final String ACCOUNT_SID = "AC76add91e6b5de53e86ad317ebedcc246";
-    public static final String AUTH_TOKEN = "b6acdab9ab349315f381ed4f95dfe586";
+    //public static final String ACCOUNT_SID = "AC76add91e6b5de53e86ad317ebedcc246";
+    //public static final String AUTH_TOKEN = "b6acdab9ab349315f381ed4f95dfe586";
 
+    @Value("${twilio.accountSid}")
+    private String ACCOUNT_SID;
+
+    @Value("${twilio.authToken}")
+    private String AUTH_TOKEN;
     public void sendSms(String numeroDestinataire, String messageBody) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
