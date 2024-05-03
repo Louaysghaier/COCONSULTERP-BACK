9fd0ec35454ea5c8c919f695b6fbb30abec75ea9
Wifekarf <wifek.arfaoui@esprit.tn>
2024-05-03 15:24:10 +0200

    wifek

--- src/main/java/com/test/COCONSULT/Entity/Pointage.java
+++ src/main/java/com/test/COCONSULT/Entity/Pointage.java
@@ -12,7 +12,7 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+//@ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Pointage implements Serializable {
 
--- src/main/java/com/test/COCONSULT/Entity/User.java
+++ src/main/java/com/test/COCONSULT/Entity/User.java
@@ -11,7 +11,7 @@ import java.util.Set;
 
 @Entity
 @AllArgsConstructor
-@ToString
+//@ToString
 @Getter
 @Setter
 @NoArgsConstructor
